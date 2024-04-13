2feccff3d1a1fcc06f1ddda82b2853228935f773
sabamoradi <saba.moradi7719@gmail.com>
2024-04-13 22:41:47 +0330

    base of project

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+'useClient'
+import './style.scss';
+
+interface Props {
+    value?: string | number;
+    inputClassName?: string;
+    placeholder: string;
+    type: string;
+    onClick?: () => void;
+};
+
+function Input(props: Props) {
+    const {
+        type,
+        value,
+        placeholder,
+        inputClassName,
+        onClick,
+    } = props;
+
+    return (
+        <div className={'container_input'}>
+            
+                <input
+                    type={type}
+                    value={value}
+                    placeholder={placeholder}
+                    className={inputClassName}
+                    onClick={onClick}
+                />
+        </div>
+    );
+};
+
+export default Input;
---
+++ src/containers/Header/index.tsx
@@ -0,0 +1,10 @@
+import Input from '@/components/Input'
+import './style.scss'
+
+export default function Header() {
+  return (
+    <header className="header_wrapper bg-white">
+        <Input placeholder='search ...' type='search'/>
+    </header> 
+  )
+}
