29ff32e674cc871758b69cfa58939e7f39c37e31
sabamoradi <saba.moradi7719@gmail.com>
2024-04-15 08:45:22 +0330

    change style of mobile menu

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -18,19 +18,29 @@ export default function Sidebar(props: Props) {
   return (
     <>
       <button
-        className="show-btn"
+        className="mobile_menu"
         onClick={() => {
           setOpenMenu(true);
         }}
       >
-        <div className="menu-line"></div>
-        <div className="menu-line"></div>
-        <div className="menu-line"></div>
+        <div>
+          <button className="relative group">
+            <div className="relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 ring-opacity-30 duration-200 shadow-md">
+              <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center">
+                <div className="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75"></div>
+                <div className="bg-white h-[1px] rounded"></div>
+                <div className="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75"></div>
+              </div>
+            </div>
+          </button>
+        </div>
       </button>
       <div className={`sidebar_container ${openMenu && "open_menu"}`}>
-        <div className="close-btn" onClick={() => {
-          setOpenMenu(false);
-        }}>close</div>
+        <button type="button"  onClick={() => {setOpenMenu(false);}} className="flex items-center justify-end text-gray-400 focus:outline-none close-btn">
+              <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+            </svg>
+        </button>
         <Link href="/">
           <p>All</p>
         </Link>
