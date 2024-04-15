0b4eb41516086b0d0c9a233f865861c7b7be3821
sabamoradi <saba.moradi7719@gmail.com>
2024-04-15 08:47:37 +0330

    fix console errors

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -17,14 +17,7 @@ export default function Sidebar(props: Props) {
 
   return (
     <>
-      <button
-        className="mobile_menu"
-        onClick={() => {
-          setOpenMenu(true);
-        }}
-      >
-        <div>
-          <button className="relative group">
+        <button className="relative group mobile_menu"  onClick={() => {setOpenMenu(true);}}>
             <div className="relative flex items-center justify-center rounded-full w-[50px] h-[50px] transform transition-all bg-slate-700 ring-0 ring-gray-300 ring-opacity-30 duration-200 shadow-md">
               <div className="flex flex-col justify-between w-[20px] h-[20px] transform transition-all duration-300 origin-center">
                 <div className="bg-white h-[2px] w-1/2 rounded transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-right delay-75"></div>
@@ -32,9 +25,8 @@ export default function Sidebar(props: Props) {
                 <div className="bg-white h-[2px] w-1/2 rounded self-end transform transition-all duration-300 group-focus:-rotate-90 group-focus:h-[1px] origin-left delay-75"></div>
               </div>
             </div>
-          </button>
-        </div>
-      </button>
+        </button>
+      
       <div className={`sidebar_container ${openMenu && "open_menu"}`}>
         <button type="button"  onClick={() => {setOpenMenu(false);}} className="flex items-center justify-end text-gray-400 focus:outline-none close-btn">
               <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
