cc488d12f9222dae83a6dcf8c735894871076c61
sabamoradi <saba.moradi7719@gmail.com>
2024-04-15 08:31:06 +0330

    change some styles to tailwind classes

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -18,26 +18,23 @@ const CustomPagination = (props: Props) => {
   const skip = params.get("skip") || 0;
 
   return (
-    <div className="pagination_wrapper">
-      <div className={`action_item ${+skip === 0 && "disable_btn"}`}>
-        {+skip > 0 ? (
-          <Link href={`?skip=${+skip - 10}`}>prev</Link>
-        ) : (
-          <p>prev</p>
-        )}
-      </div>
-      <div className="page_item">
-          <span>
-            {(+skip /10) + 1}
-          </span>
-      </div>
-      <div className={`action_item ${+total === (+skip) + 10 && "disable_btn"}`}>
-        {+total === (+skip) + 10 ? (
-          <p>next</p>
-        ) : (
-          <Link href={`?skip=${+skip + 10}`}>next</Link>
-        )}
-      </div>
+    <div className="flex p-2 justify-center">
+      <Link href={+skip - 10 === 0 ? '/' : `?skip=${+skip - 10}`} className={`flex items-center justify-center px-4 me-3 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg ${+skip === 0 && "disable_btn"}`}>
+        <svg className="w-3.5 h-3.5 me-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H1m0 0 4 4M1 5l4-4"/>
+        </svg>
+        <p>
+          Previous
+        </p>
+      </Link>
+      <Link href={+total === (+skip) + 10 ? '#' : `?skip=${+skip + 10}`} className={`flex items-center justify-center px-4 h-10 text-base font-medium text-gray-500 bg-white border border-gray-300 rounded-lg ${+total === (+skip) + 10 && "disable_btn"}`}>
+        <p>
+          Next
+        </p>
+        <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+        </svg>
+      </Link>
     </div>
   );
 };
